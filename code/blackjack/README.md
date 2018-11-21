49c95d71347db2fec940d9e8a2e94ecd0598db79
hopkinsm <hopkinsm@C02X456YJHD3.local>
2018-11-21 10:16:45 -0800

    Added documentation.

---
+++ code/blackjack/README.md
@@ -0,0 +1,8 @@
+Learning to Play Blackjack Using Reinforcement Learning
+-------------------------------------------------------
+
+To run the simulator, type:
+
+    python blackjack.py
+    
+in a console.
--- code/blackjack/blackjack.py
+++ code/blackjack/blackjack.py
@@ -1,127 +1,142 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 29 18:29:37 2018
+from pgl import GWindow, GLabel, GRect
+from qlearning import QLearningAgent
+from environment import BlackjackEnvironment
 
-@author: hopkinsm
-"""
+GWINDOW_WIDTH = 1000               # Width of the graphics window
+GWINDOW_HEIGHT = 500              # Height of the graphics window
+LETTER_BASE = 10                  # Distance from bottom to the letters
+WORD_BASE = 45                    # Distance from bottom to secret word
+MESSAGE_BASE = 85                 # Distance from bottom to message area
+SNOWMAN_BASE = 140                # Distance from bottom to Snowman base
+MAX_INCORRECT_GUESSES = 8         # Number of incorrect guesses allowed
+INCORRECT_COLOR = "#FF9999"       # Color used for incorrect guesses
+CORRECT_COLOR = "#009900"         # Color used to mark correct guesses
 
-import random
-from collections import defaultdict
+# Fonts
 
+WORD_FONT = "bold 36px 'Monaco','Monospaced'"
+LETTER_FONT = "bold 24px 'Monaco','Monospaced'"
+MESSAGE_FONT = "30px 'Helvetica Neue','Arial','Sans-Serif'"
+gw = GWindow(GWINDOW_WIDTH, GWINDOW_HEIGHT)
+
+
+env = BlackjackEnvironment()
+agent = QLearningAgent(env)
+
+def createWindow():
+ 
     
+    def createCardTotalLabels():
+        alphabet = ['02','03','04','05','06','07','08','09','10',
+                    '11','12','13','14','15','16','17','18','19',
+                    '20','21']
+        alphabetLabels = [GLabel(letter) for letter in alphabet]
+        for label in alphabetLabels:
+            label.setFont(LETTER_FONT)    
+        return alphabetLabels
 
-class QLearningAgent():
-  
-    def __init__(self, env, epsilon=0.4, discount = 1.0, epochBatchSize=5000):
-        self.qValues = defaultdict(int)
-        self.env = env
-        self.epsilon = epsilon
-        self.discount = discount
-        self.qaCount = defaultdict(int)
-        self.epochBatchSize = 5000
-        self.epochsSoFar = 0
-        self.weights = defaultdict(int)
-        self.states = {state: i for (i, state) in enumerate(self.env.getStates())}
 
-    def go(self):
-        for i in range(self.epochBatchSize):
-            self.env.reset()
-            while(len(self.env.getPossibleActions(self.env.getCurrentState())) > 0):
-                state = self.env.getCurrentState()
-                action = self.getAction(state)
-                (nextState, reward) = self.env.doAction(action)
-                self.update(state, action, nextState, reward)
-        self.epochsSoFar += self.epochBatchSize
-            
-    def getWeights(self):
-        return self.weights
-
-    def getFeatures(self, state, action):
-        featname = '{}-{}'.format(state,action)
-        feats = {featname: 1.0}
-        return feats
-        
-    def getQValue(self, state, action):
-        """
-          Should return Q(state,action) = w * featureVector
-          where * is the dotProduct operator
-        """
-        featureVector = self.getFeatures(state, action)
-        dotProduct = 0.0
-        for featureName in self.weights:
-            if featureName in featureVector:
-                weight = self.weights[featureName]
-                featureValue = featureVector[featureName]
-                dotProduct += weight * featureValue
-        return dotProduct
-
-    def _computeValueActionPairFromQValues(self, state):
-        legalActions = self.env.getPossibleActions(state)
-        if len(legalActions) == 0:
-            return (0.0, None)
-        else:
-            qVals = [self.getQValue(state, action) for action in legalActions]
-            return max(zip(qVals, legalActions))
-
-    def getValue(self, state):
-        """
-          Returns max_action Q(state,action)
-          where the max is over legal actions.  Note that if
-          there are no legal actions, which is the case at the
-          terminal state, you should return a value of 0.0.
-        """
-        return self._computeValueActionPairFromQValues(state)[0]
-
-    def getPolicy(self, state):
-        """
-          Compute the best action to take in a state.  Note that if there
-          are no legal actions, which is the case at the terminal state,
-          you should return None.
-        """
-        return self._computeValueActionPairFromQValues(state)[1]
-            
-
-    def getAction(self, state):
-        """
-          Compute the action to take in the current state.  With
-          probability self.epsilon, we should take a random action and
-          take the best policy action otherwise.  Note that if there are
-          no legal actions, which is the case at the terminal state, you
-          should choose None as the action.
-        """
+    def createUpCardLabels():
+        alphabet = ['01','02','03','04','05','06','07','08','09','10']
+        alphabetLabels = [GLabel(letter) for letter in alphabet]
+        for label in alphabetLabels:
+            label.setFont(LETTER_FONT)           
+        return alphabetLabels
     
-        def flipCoin(eps):    
-            r = random.random()
-            return r < eps
-
-        
-        # Pick Action
-        legalActions = self.env.getPossibleActions(state)
-        
-        if len(legalActions) == 0:
-            return None
-        elif flipCoin(self.epsilon):
-            return random.choice(legalActions)
-        else:
-            return self.getAction(state)
+     
+    def makeDisplay(alphabetLabels, upCardLabels):
+        decisionIndicators = dict()
+        letterWidth = alphabetLabels[0].getWidth()
+        letterHeight = alphabetLabels[0].getHeight()
+        XMARGIN = 50
+        offset = (GWINDOW_WIDTH - XMARGIN -
+                  (len(alphabetLabels) * letterWidth)) / (len(alphabetLabels) + 1)
+        for (i, label) in enumerate(alphabetLabels):
+            x = XMARGIN + (offset + letterWidth) * i + offset
+            gw.add(label, x, GWINDOW_HEIGHT - LETTER_BASE)
             
-    def update(self, state, action, nextState, reward):
-        """
-           Should update your weights based on transition
-        """
-        maxQ = self._computeValueActionPairFromQValues(nextState)[0]
-        self.qaCount[(state, action)] += 1
-        alpha = 1. / self.qaCount[(state, action)]
-        multiplier = alpha * (
-                reward 
-                + (self.discount * maxQ) 
-                - self.getQValue(state, action))
-        featureVector = self.getFeatures(state, action)
-        for featureName in featureVector:
-            featureValue = featureVector[featureName]
-            self.weights[featureName] = (self.weights[featureName] + 
-                        multiplier * 
-                        featureValue)
+        YMARGIN = 100        
+        upCardOffset = (GWINDOW_HEIGHT - YMARGIN - 
+                  (len(upCardLabels) * letterHeight)) / (len(upCardLabels) + 1)
+        for (i, label) in enumerate(upCardLabels):
+            y = YMARGIN + (upCardOffset + letterHeight) * i + upCardOffset
+            gw.add(label, LETTER_BASE, y)
 
+        for (i, upCardLabel) in enumerate(upCardLabels):
+            y = YMARGIN + (upCardOffset + letterHeight) * i + upCardOffset - letterHeight
+         
+            for (j, totalLabel) in enumerate(alphabetLabels):
+                x = XMARGIN + (offset + letterWidth) * j + offset
+        
+                decisionIndicator = GRect(x, y, letterHeight, letterWidth)
+                decisionIndicator.setFillColor("black")
+                decisionIndicator.setFilled(True)
+                gw.add(decisionIndicator)
+                decisionIndicators[(str(int(totalLabel.getLabel())), 
+                                    str(int(upCardLabel.getLabel())))] = decisionIndicator
+        return decisionIndicators
+            
+    cardTotalLabels  = createCardTotalLabels()
+    upCardLabels = createUpCardLabels()
+    return makeDisplay(cardTotalLabels, upCardLabels)
 
+def runSimulation():
+
+    decisionIndicators = createWindow()
+    progressLabel = GLabel("GAMES PLAYED: 0")
+    progressLabel.setFont(MESSAGE_FONT)
+    gw.add(progressLabel, 20, 50)
+    
+    def adjustIndicators():
+        progressLabel.setLabel('GAMES PLAYED: {}'.format(agent.epochsSoFar))
+        for state in decisionIndicators:
+            action = agent.getPolicy(state)
+            indicator = decisionIndicators[state]
+            if action == 'hit':
+                indicator.setFillColor("green")
+                indicator.setFilled(True)
+            else:
+                indicator.setFillColor("red")
+                indicator.setFilled(True)
+    
+    def adjustIndicatorsAlt():
+        progressLabel.setLabel('GAMES PLAYED: {}'.format(agent.epochsSoFar))
+        for state in decisionIndicators:
+            hitValue = agent.getQValue(state, 'hit')
+            standValue = agent.getQValue(state, 'stand')
+            hitDifferential = hitValue - standValue
+            if hitDifferential > 1:
+                color = "#00FF00"
+            elif hitDifferential > 0.5:
+                color = "#00CC00"
+            elif hitDifferential > 0.2:
+                color = "#008800"
+            elif hitDifferential > 0.1:
+                color = "#006600"
+            elif hitDifferential > 0.05:
+                color = "#004400"
+            elif hitDifferential > -0.05:
+                color = "#000000"
+            elif hitDifferential > 0.1:
+                color = "#440000"
+            elif hitDifferential > -0.2:
+                color = "#660000"
+            elif hitDifferential > -0.5:
+                color = "#880000"
+            elif hitDifferential > -1:
+                color = "#CC0000"
+            else:
+                color = "#FF0000"
+            indicator = decisionIndicators[state]
+            indicator.setFillColor(color)
+            indicator.setFilled(True)
+    
+    timer1 = gw.createTimer(agent.go, 1000)
+    timer1.setRepeats(True)
+    timer1.start()
+    timer2 = gw.createTimer(adjustIndicatorsAlt, 1500)
+    timer2.setRepeats(True)
+    timer2.start()
+
+if __name__ == '__main__':
+    runSimulation()
